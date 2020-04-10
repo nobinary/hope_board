3d58b722afe91c1f5d182a82ea1bd0c59f975ef4
Melissa Donegan <melissadonegan25@gmail.com>
2020-04-10 18:37:10 -0400

    added initial html for login page

--- client/src/Screens/Login.jsx
+++ client/src/Screens/Login.jsx
@@ -1,14 +1,48 @@
 import React from 'react'
 import BigPostIt from '../Shared Components/BigPostIt'
+import '../Style/Login.scss'
 
 function Login() {
-    return (
-      <div className="note_main">
-          <BigPostIt 
-          type="login"
-          />
-      </div>
-    );
-  }
-  
-  export default Login;
+  return (
+    <div class="note_main">
+      {/* <BigPostIt
+        type="login"
+      /> */}
+      <main id="login-form">
+        <p>Welcome Back!</p>
+        <h1>Login</h1>
+        <form>
+          <label for="username">
+            Username:
+            <input
+              type="text"
+              placeholder="Enter your username"
+              id="username"
+              required
+            >
+            </input>
+          </label>
+          <label for="username">
+            Password:
+            <input
+              type="text"
+              placeholder="Enter your password"
+              id="password"
+              required
+            >
+            </input>
+          </label>
+        <button 
+        type="submit" 
+        id="submit"
+        >
+          Submit
+          </button>
+        </form>
+
+      </main>
+    </div>
+  );
+}
+
+export default Login;
