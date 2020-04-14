236d7ae46c039d7a889a8c14ae13ae6b3dfd8433
M.K. Irvine <irvinemk@gmail.com>
2020-04-14 10:41:54 -0400

    jr-mk rev

--- client/src/Components/Note.jsx
+++ client/src/Components/Note.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../Style/Note.scss'
+import axios from 'axios'
 
 // pass API props down from Board/MyBoard mapping
 function Note(props) {
@@ -13,3 +14,11 @@ function Note(props) {
   }
   
   export default Note;
+
+//   id: 50,
+// color: "blue",
+// content: "Portland literally post-ironic normcore.",
+// num_likes: 1,
+// created_at: "2020-04-13T17:41:14.472Z",
+// updated_at: "2020-04-13T17:41:14.931Z",
+// user_id: 10
--- client/src/Screens/Board.jsx
+++ client/src/Screens/Board.jsx
@@ -2,23 +2,42 @@ import React from "react";
 import Menu from "../Components/Menu";
 import Note from "../Components/Note";
 import Footer from "../Components/Footer";
+import Axios from 'axios'
 import "../Style/Board.scss";
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      notes: []
+    };
   }
+
+  componentDidMount() {
+    this.fetchNotes();
+  }
+
+  fetchNotes = async () => {
+    try {
+    const notes = await Axios.get('http://localhost:3000/notes')
+    console.log(notes)
+    } catch (error) {
+      console.log("Error: ", error)
+    }
+  }
+
+
   render() {
     return (
       <div id="fp_main">
-        <img id="background" class="img-responsive" src="https://i.imgur.com/PYvrnAk.jpg" alt="brown corkboard background" />
+        <img id="background" className="img-responsive" src="https://i.imgur.com/PYvrnAk.jpg" alt="brown corkboard background" />
           <div class="board-menu">
             <Menu />
           </div>
-          <div class="board-box">
+          <div className="board-box">
             {/* <p>This is THE BOARD</p> */}
             {/* INSERT API/RENDER NOTEs */}
+        
             <div className="notes">
               <Note />
               <Note />
