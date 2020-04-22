3c8c6edfd04c2c96dcc0084cd609c6bf4856f2a3
Melissa Donegan <melissadonegan25@gmail.com>
2020-04-22 19:42:54 -0400

    implemented same functionality and initial layout for AboutModal

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 // import FPButton from '../Shared Components/FPButton'
 import "../Style/Footer.scss"
 import NoteModal from '../Components/NoteModal'
+import AboutModal from '../Components/AboutModal'
 import {Link} from 'react-router-dom'
 
 function Footer() {
     return (
       <div id="footer_main">
           <NoteModal />
-          <img 
+          <AboutModal />
+          {/* <img 
           src="https://i.imgur.com/JgBnwAE.png" 
           id="about-icon" 
           className="img-responsive"
           alt="white info icon that directs you to how website works" 
-          />
+          /> */}
       </div>
     );
   }
