752e62b51c0da7e25b8c352c565185265762038f
Melissa Donegan <melissadonegan25@gmail.com>
2020-09-04 16:55:46 -0400

    added surge link

---
+++ client/build/precache-manifest.09c1616ef11338dd8a24643f50d2deb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de48a6d269d027d19def61b51072f7a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b4e1b8b31fb09427b8d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "718d3363c7b767ebb564",
+    "url": "/static/css/main.3f83e24c.chunk.css"
+  },
+  {
+    "revision": "1b4e1b8b31fb09427b8d",
+    "url": "/static/js/2.acc60695.chunk.js"
+  },
+  {
+    "revision": "99eecbe0e7fc6292d8355d7acee57e18",
+    "url": "/static/js/2.acc60695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "718d3363c7b767ebb564",
+    "url": "/static/js/main.6dc5ade5.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
